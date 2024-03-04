42d45e3c0787851dc15ace149bfd79bef1d5ac80
Anirudh Belwadi <anirudh.belwadi@gmail.com>
2024-03-04 10:59:30 +0530

    hsn structure code added

--- src/product-categories/hsn-format/index.js
+++ src/product-categories/hsn-format/index.js
@@ -1,13 +1,55 @@
 import './style.scss';
 import Image from 'next/image';
-import hsnFormat from '../../../public/images/product-categories/hsn-format.png';
+import formatSeparator from '../../../public/images/product-categories/format-separator.png';
 
 const HSNFormat = () => {
   return (
     <>
       <div className='hsn-format'>
         <p className='hsn-format__floating-left'>HSN</p>
-        <Image src={hsnFormat} alt='hsn-format' className='hsn-format__img' />
+        <div className='hsn-format__structure'>
+          <div className='hsn-format__structure-bg-line'></div>
+          <div className='hsn-format__structure-description'>
+            <div className='hsn-format__structure-description__code-container'>
+              <span>1</span>
+              <span>1</span>
+            </div>
+            <div className='hsn-format__structure-description__arrow-img'>
+              <Image src={formatSeparator} alt='format separator' />
+              <p>Chapter</p>
+            </div>
+          </div>
+          <div className='hsn-format__structure-description'>
+            <div className='hsn-format__structure-description__code-container'>
+              <span>1</span>
+              <span>1</span>
+            </div>
+            <div className='hsn-format__structure-description__arrow-img'>
+              <Image src={formatSeparator} alt='format separator' />
+              <p>Heading</p>
+            </div>
+          </div>
+          <div className='hsn-format__structure-description'>
+            <div className='hsn-format__structure-description__code-container'>
+              <span>1</span>
+              <span>1</span>
+            </div>
+            <div className='hsn-format__structure-description__arrow-img'>
+              <Image src={formatSeparator} alt='format separator' />
+              <p>Sub-Heading</p>
+            </div>
+          </div>
+          <div className='hsn-format__structure-description'>
+            <div className='hsn-format__structure-description__code-container'>
+              <span>1</span>
+              <span>1</span>
+            </div>
+            <div className='hsn-format__structure-description__arrow-img'>
+              <Image src={formatSeparator} alt='format separator' />
+              <p>Tariff Item</p>
+            </div>
+          </div>
+        </div>
       </div>
       <p className='hsn-format__floating-right'>STRUCTURE</p>
     </>
