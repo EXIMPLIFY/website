024823442c7ee2a02ed014b3d1c3df8f63ca3021
vijay-navale <vijaynavale.dev@gmail.com>
2024-03-23 21:18:43 +0530

    Completed additional info section in HSN kundali

--- src/hsn-kundali/additional-info/index.js
+++ src/hsn-kundali/additional-info/index.js
@@ -1,52 +1,94 @@
 import './style.scss';
+import { useState } from 'react';
 import SectionLabel from '../../commons/section-label';
 import ButtonTransparent from '../../commons/buttons/button-transparent';
+import useDeviceSize from '../../hooks/use-device-size';
+
+const data = [
+  {
+    heading: 'Licences',
+    subHead:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's..."
+  },
+  {
+    heading: 'Associated Associations',
+    subHead:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's..."
+  }
+];
 
 const AdditionalInfo = () => {
+  const { width } = useDeviceSize();
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const setActiveItemIndex = index => {
+    if (activeIndex === index) {
+      setActiveIndex(null);
+      return;
+    }
+    setActiveIndex(index);
+  };
+
   return (
     <section className='additionalInfo'>
       <SectionLabel title='Additional Info' />
       <div className='additionalInfo__accordion-panel'>
-        <div className='additionalInfo__accordion-item'>
-          <div>
-            <span className='additionalInfo__item-no'>01</span>
-            <h5 className='additionalInfo__item-heading'>Licences</h5>
-            <p className='section-description'>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's...{' '}
-              <span>
-                standard dummy text ever since the 1500s, when an unknown
-                printer took a galley of type and scrambled it to make a type
-                specimen book. It has survived not only five centuries, but also
-                the leap into electronic typesetting, remaining essentially
-                unchanged.
+        {data.map(({ heading, subHead }, index) => (
+          <div
+            className='additionalInfo__accordion-item'
+            key={heading}
+            onClick={() => setActiveItemIndex(index)}
+          >
+            <div className='additionalInfo__accordion-item-content-wrapper'>
+              <span className='additionalInfo__item-no'>
+                {String(index).length === 1 ? `0${index + 1}` : index + 1}
               </span>
-            </p>
-            <ButtonTransparent title='Login to Know More' />
-          </div>
-          <span className='additionalInfo__item-btn'>+</span>
-        </div>
-        <div className='additionalInfo__accordion-item'>
-          <div>
-            <span className='additionalInfo__item-no'>02</span>
-            <h5 className='additionalInfo__item-heading'>
-              Associated Associations
-            </h5>
-            <p className='section-description'>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's...{' '}
-              <span>
-                standard dummy text ever since the 1500s, when an unknown
-                printer took a galley of type and scrambled it to make a type
-                specimen book. It has survived not only five centuries, but also
-                the leap into electronic typesetting, remaining essentially
-                unchanged.
+              <h5
+                className={`additionalInfo__item-heading ${
+                  activeIndex === index ? 'active' : ''
+                }`}
+              >
+                {heading}
+              </h5>
+              <div
+                className={
+                  activeIndex === index ? 'show-content' : 'hide-content'
+                }
+              >
+                <p className='section-description'>
+                  {subHead}
+                  <span>
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged.
+                  </span>
+                </p>
+                {width > 540 && (
+                  <ButtonTransparent title='Login to Know More' />
+                )}
+              </div>
+            </div>
+            <div className='additionalInfo__mb-btn-wrapper'>
+              {width <= 540 && (
+                <ButtonTransparent
+                  title='Login to Know More'
+                  className={activeIndex === index ? 'show-btn' : 'hide-btn'}
+                />
+              )}
+              <span
+                className={
+                  activeIndex === index
+                    ? 'additionalInfo__item-btn active'
+                    : 'additionalInfo__item-btn'
+                }
+              >
+                +
               </span>
-            </p>
-            <ButtonTransparent title='Login to Know More' />
+            </div>
           </div>
-          <span className='additionalInfo__item-btn'>+</span>
-        </div>
+        ))}
       </div>
     </section>
   );
